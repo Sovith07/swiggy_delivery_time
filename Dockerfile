cb84604fcf23b6eaf58e28572d3b8af26304850e
Sovith <sovithkumarsingh786786@gmail.com>
2026-09-03 18:56:19 +0530

    test model performance and promote model

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,25 @@
+# set the base image
 FROM python:3.12-slim
 
+# install lightgbm dependency
+RUN apt-get update && apt-get install -y libgomp1
+
+# set up the working directory
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y libgomp1 && rm -rf /var/lib/apt/lists/*
 
+
+# copy the app contents
 COPY flask_app/ /app/
-COPY requirements.txt /app/requirements.txt
+COPY ./models/preprocessor.joblib ./models/preprocessor.joblib
+COPY ./run_information.json ./
+# install the packages
+RUN pip install -r dev_requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 5000
 
-CMD ["python", "app.py"]
+# expose the port
+EXPOSE 8000
+
+# Run the file using command
+CMD [ "python","./app.py" ]
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,26 +1,351 @@
-import pickle
-
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+import uvicorn
 import pandas as pd
-from flask import Flask, jsonify, request
+import mlflow
+import json
+import joblib
+from mlflow import MlflowClient
+from sklearn import set_config
+import numpy as np
 
-app = Flask(__name__)
+# set the output as pandas
+set_config(transform_output='pandas')
 
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
+# initialize dagshub
+import dagshub
+import mlflow.client
 
+import dagshub
+dagshub.init(repo_owner='Sovith07', repo_name='swiggy_delivery_time', mlflow=True)
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json()
-    df = pd.DataFrame(data if isinstance(data, list) else [data])
-    preds = model.predict(df)
-    return jsonify(preds.tolist())
-
-
-@app.route("/health", methods=["GET"])
-def health():
-    return jsonify({"status": "ok"})
+# set the mlflow tracking server
+mlflow.set_tracking_uri("https://dagshub.com/Sovith07/swiggy_delivery_time.mlflow")
 
 
+class Data(BaseModel):  
+    ID: str
+    Delivery_person_ID: str
+    Delivery_person_Age: str
+    Delivery_person_Ratings: str
+    Restaurant_latitude: float
+    Restaurant_longitude: float
+    Delivery_location_latitude: float
+    Delivery_location_longitude: float
+    Order_Date: str
+    Time_Orderd: str
+    Time_Order_picked: str
+    Weatherconditions: str
+    Road_traffic_density: str
+    Vehicle_condition: int
+    Type_of_order: str
+    Type_of_vehicle: str
+    multiple_deliveries: str
+    Festival: str
+    City: str
+
+    
+    
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info = json.load(f)
+        
+    return run_info
+
+
+def load_transformer(transformer_path):
+    transformer = joblib.load(transformer_path)
+    return transformer
+
+
+
+# columns to preprocess in data
+num_cols = ["age",
+            "ratings",
+            "pickup_time_minutes",
+            "distance"]
+
+nominal_cat_cols = ['weather',
+                    'type_of_order',
+                    'type_of_vehicle',
+                    "festival",
+                    "city_type",
+                    "is_weekend",
+                    "order_time_of_day"]
+
+ordinal_cat_cols = ["traffic","distance_type"]
+
+#mlflow client
+client = MlflowClient()
+
+# load the model info to get the model name
+model_uri = load_model_information("run_information.json")['model_uri']
+
+# stage of the model
+stage = "Production"
+
+# get the latest model version
+# latest_model_ver = client.get_latest_versions(name=model_name,stages=[stage])
+# print(f"Latest model in production is version {latest_model_ver[0].version}")
+
+# load model path
+
+
+model = mlflow.pyfunc.load_model(model_uri)
+
+# load the preprocessor
+preprocessor_path = "models/preprocessor.joblib"
+preprocessor = load_transformer(preprocessor_path)
+
+
+
+def change_column_names(data: pd.DataFrame) -> pd.DataFrame:
+    return (
+        data.rename(str.lower,axis=1)
+        .rename({
+            "delivery_person_id" : "rider_id",
+            "delivery_person_age": "age",
+            "delivery_person_ratings": "ratings",
+            "delivery_location_latitude": "delivery_latitude",
+            "delivery_location_longitude": "delivery_longitude",
+            "time_orderd": "order_time",
+            "time_order_picked": "order_picked_time",
+            "weatherconditions": "weather",
+            "road_traffic_density": "traffic",
+            "city": "city_type"},axis=1)
+    )
+
+
+def data_cleaning(data: pd.DataFrame) -> pd.DataFrame:
+    minors_data = data.loc[data['age'].astype('float') < 18]
+    minor_index = minors_data.index.tolist()
+    six_star_data = data.loc[data['ratings'] == "6"]
+    six_star_index = six_star_data.index.tolist()
+
+    return (
+        data
+        .drop(columns="id")
+        .drop(index=minor_index)                                                # Minor riders in data dropped
+        .drop(index=six_star_index)                                             # six star rated drivers dropped
+        .replace("NaN ",np.nan)                                                 # missing values in the data
+        .assign(
+            # city column out of rider id
+            city_name = lambda x: x['rider_id'].str.split("RES").str.get(0),
+            # convert age to float
+            age = lambda x: x['age'].astype(float),
+            # convert ratings to float
+            ratings = lambda x: x['ratings'].astype(float),
+            # absolute values for location based columns
+            restaurant_latitude = lambda x: x['restaurant_latitude'].abs(),
+            restaurant_longitude = lambda x: x['restaurant_longitude'].abs(),
+            delivery_latitude = lambda x: x['delivery_latitude'].abs(),
+            delivery_longitude = lambda x: x['delivery_longitude'].abs(),
+            # order date to datetime and feature extraction
+            order_date = lambda x: pd.to_datetime(x['order_date'],
+                                                  format='%d-%m-%Y'),
+            order_day = lambda x: x['order_date'].dt.day,
+            order_month = lambda x: x['order_date'].dt.month,
+            order_day_of_week = lambda x: x['order_date'].dt.day_name().str.lower(),
+            is_weekend = lambda x: (x['order_date']
+                                    .dt.day_name()
+                                    .isin(["Saturday","Sunday"])
+                                    .astype(int)),
+            # time based columns
+            order_time = lambda x: pd.to_datetime(x['order_time'],
+                                                  format='mixed'),
+            order_picked_time = lambda x: pd.to_datetime(x['order_picked_time'],
+                                                         format='mixed'),
+            # time taken to pick order
+            pickup_time_minutes = lambda x: (
+                                            (x['order_picked_time'] - x['order_time'])
+                                            .dt.seconds / 60
+                                            ),
+            # hour in which order was placed
+            order_time_hour = lambda x: x['order_time'].dt.hour,
+            # time of the day when order was placed
+            order_time_of_day = lambda x: (
+                                x['order_time_hour'].pipe(time_of_day)),
+            # categorical columns
+            weather = lambda x: (
+                                x['weather']
+                                .str.replace("conditions ","")
+                                .str.lower()
+                                .replace("nan",np.nan)),
+            traffic = lambda x: x["traffic"].str.rstrip().str.lower(),
+            type_of_order = lambda x: x['type_of_order'].str.rstrip().str.lower(),
+            type_of_vehicle = lambda x: x['type_of_vehicle'].str.rstrip().str.lower(),
+            festival = lambda x: x['festival'].str.rstrip().str.lower(),
+            city_type = lambda x: x['city_type'].str.rstrip().str.lower(),
+            # multiple deliveries column
+            multiple_deliveries = lambda x: x['multiple_deliveries'].astype(float))
+           .drop(columns=["order_time","order_picked_time"])
+    )
+    
+    
+    
+def clean_lat_long(data: pd.DataFrame, threshold: float=1.0) -> pd.DataFrame:
+    location_columns = ['restaurant_latitude',
+                        'restaurant_longitude',
+                        'delivery_latitude',
+                        'delivery_longitude']
+
+    return (
+        data
+        .assign(**{
+            col: (
+                np.where(data[col] < threshold, np.nan, data[col].values)
+            )
+            for col in location_columns
+        })
+    )
+    
+    
+# extract day, day name, month and year
+def extract_datetime_features(ser: pd.Series) -> pd.DataFrame:
+    date_col = pd.to_datetime(ser,dayfirst=True)
+
+    return (
+        pd.DataFrame(
+            {
+                "day": date_col.dt.day,
+                "month": date_col.dt.month,
+                "year": date_col.dt.year,
+                "day_of_week": date_col.dt.day_name(),
+                "is_weekend": date_col.dt.day_name().isin(["Saturday","Sunday"]).astype(int)
+            }
+        ))
+    
+
+    
+def time_of_day(ser: pd.Series):
+
+    return(
+        pd.cut(ser,bins=[0,6,12,17,20,24],right=True,
+               labels=["after_midnight","morning","afternoon","evening","night"])
+    )
+
+
+
+def calculate_haversine_distance(df: pd.DataFrame) -> pd.DataFrame:
+    location_columns = ['restaurant_latitude',
+                        'restaurant_longitude',
+                        'delivery_latitude',
+                        'delivery_longitude']
+    
+    lat1 = df[location_columns[0]]
+    lon1 = df[location_columns[1]]
+    lat2 = df[location_columns[2]]
+    lon2 = df[location_columns[3]]
+
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(
+        dlat / 2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    distance = 6371 * c
+
+    return (
+        df.assign(
+            distance = distance)
+    )
+
+
+def create_distance_type(data: pd.DataFrame) -> pd.DataFrame:
+    return(
+        data
+        .assign(
+                distance_type = pd.cut(data["distance"],bins=[0,5,10,15,25],
+                                        right=False,labels=["short","medium","long","very_long"])
+    ))
+
+
+
+def drop_columns(data: pd.DataFrame, columns: list) -> pd.DataFrame:
+    df = data.drop(columns=columns)
+    return df
+ 
+columns_to_drop =  ['rider_id',
+                    'restaurant_latitude',
+                    'restaurant_longitude',
+                    'delivery_latitude',
+                    'delivery_longitude',
+                    'order_date',
+                    "order_time_hour",
+                    "order_day",
+                    "city_name",
+                    "order_day_of_week",
+                    "order_month"]   
+    
+def perform_data_cleaning(data: pd.DataFrame) -> pd.DataFrame:
+
+    cleaned_data = (
+            data
+            .pipe(change_column_names)
+            .pipe(data_cleaning)
+            .pipe(clean_lat_long)
+            .pipe(calculate_haversine_distance)
+            .pipe(create_distance_type)
+            .pipe(drop_columns,columns=columns_to_drop)
+        )
+    return cleaned_data
+# create the app
+app = FastAPI()
+
+# create the home endpoint
+@app.get(path="/")
+def home():
+    return "Welcome to the Swiggy Food Delivery Time Prediction App"
+
+# create the predict endpoint
+@app.post(path="/predict")
+def do_predictions(data: Data):
+    pred_data = pd.DataFrame({
+        'ID': data.ID,
+        'Delivery_person_ID': data.Delivery_person_ID,
+        'Delivery_person_Age': data.Delivery_person_Age,
+        'Delivery_person_Ratings': data.Delivery_person_Ratings,
+        'Restaurant_latitude': data.Restaurant_latitude,
+        'Restaurant_longitude': data.Restaurant_longitude,
+        'Delivery_location_latitude': data.Delivery_location_latitude,
+        'Delivery_location_longitude': data.Delivery_location_longitude,
+        'Order_Date': data.Order_Date,
+        'Time_Orderd': data.Time_Orderd,
+        'Time_Order_picked': data.Time_Order_picked,
+        'Weatherconditions': data.Weatherconditions,
+        'Road_traffic_density': data.Road_traffic_density,
+        'Vehicle_condition': data.Vehicle_condition,
+        'Type_of_order': data.Type_of_order,
+        'Type_of_vehicle': data.Type_of_vehicle,
+        'multiple_deliveries': data.multiple_deliveries,
+        'Festival': data.Festival,
+        'City': data.City
+        },index=[0]
+    )
+
+    # clean the raw input data
+    cleaned_data = perform_data_cleaning(pred_data)
+    processed_data = preprocessor.transform(cleaned_data)
+
+    # Convert transformed numpy array back to DataFrame
+    processed_data = pd.DataFrame(
+    processed_data,
+    columns=preprocessor.get_feature_names_out()
+)
+    processed_data["vehicle_condition"] = (
+    processed_data["vehicle_condition"].astype("int64")
+)
+    
+    # get the predictions
+    predictions = model.predict(processed_data)[0]
+    print("Prediction:", predictions)
+
+    return predictions
+   
+   
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    uvicorn.run(app="app:app",host="0.0.0.0",port=8000,reload=True)
